92c61a63f556765acacb0ca48cdc54a650df3d60
altzbox <99175896+altzbox@users.noreply.github.com>
2024-01-18 23:08:48 +0300

    Update motors_sync.py

--- motors_sync.py
+++ motors_sync.py
@@ -5,68 +5,36 @@ import numpy as np
 import time
 
 def calculate_magnitude(accel_x, accel_y, accel_z):
-    return np.sqrt(accel_x**2 + accel_y**2 + accel_z**2)
+    return np.sqrt(accel_x**2 + accel_y**2 + accel_z**2) # Remove the axis that is perpendicular to the ground.
 
 def home_printhead():
-    try:
-        # Run the command to home the printhead
-        subprocess.run(["echo _HOME_XY_AND_MOVE_TO_CENTER > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
-        print("Printhead homed successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error homing printhead: {e}")
-
+    subprocess.run(["echo _HOME_XY_AND_MOVE_TO_CENTER > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
+    
+def resume_cmd():
+    subprocess.run(["echo RESUME > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
+    
 def activate_and_measure_x():
-    try:
-        # Run the command to activate and measure X
-        subprocess.run(["echo _ACTIVATE_AND_MEASURE_X > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
-        print("ACTIVATE_AND_MEASURE_X command sent successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error sending ACTIVATE_AND_MEASURE_X command: {e}")
-
+    subprocess.run(["echo _ACTIVATE_AND_MEASURE_X > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
+    
 def activate_and_measure_y():
-    try:
-        # Run the command to activate and measure y
-        subprocess.run(["echo _ACTIVATE_AND_MEASURE_Y > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
-        print("ACTIVATE_AND_MEASURE_Y command sent successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error sending ACTIVATE_AND_MEASURE_Y command: {e}")
-
+    subprocess.run(["echo _ACTIVATE_AND_MEASURE_Y > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
+    
 def force_move_xoneplus(steps):
-    try:
-        # Run the command to force move X
-        for _ in range(steps):
-            subprocess.run(["echo _FORCE_MOVE_XONEPLUS > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
-            print("FORCE_MOVE_XONEPLUS command sent successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error sending FORCE_MOVE_XONEPLUS command: {e}")
-
+    for _ in range(steps):
+        subprocess.run(["echo _FORCE_MOVE_XONEPLUS > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
+    
 def force_move_xoneminus(steps):
-    try:
-        # Run the command to force move X
-        for _ in range(steps):
-            subprocess.run(["echo _FORCE_MOVE_XONEMINUS > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
-            print("FORCE_MOVE_XONEMINUS command sent successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error sending FORCE_MOVE_XONEMINUS command: {e}")
-
+    for _ in range(steps):
+        subprocess.run(["echo _FORCE_MOVE_XONEMINUS > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
+    
 def force_move_yoneplus(steps):
-    try:
-        # Run the command to force move Y
-        for _ in range(steps):
-            subprocess.run(["echo _FORCE_MOVE_YONEPLUS > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
-            print("FORCE_MOVE_YONEPLUS command sent successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error sending FORCE_MOVE_YONEPLUS command: {e}")
-
+    for _ in range(steps):
+        subprocess.run(["echo _FORCE_MOVE_YONEPLUS > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
+    
 def force_move_yoneminus(steps):
-    try:
-        # Run the command to force move Y
-        for _ in range(steps):
-            subprocess.run(["echo _FORCE_MOVE_YONEMINUS > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
-            print("FORCE_MOVE_YONEMINUS command sent successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error sending FORCE_MOVE_YONEMINUS command: {e}")
-
+    for _ in range(steps):
+        subprocess.run(["echo _FORCE_MOVE_YONEMINUS > ~/printer_data/comms/klippy.serial"], check=True, shell=True)
+    
 def process_generated_csv(directory_path='/tmp'):
     try:
         # Get the list of all CSV files in the directory
@@ -87,14 +55,14 @@ def process_generated_csv(directory_path='/tmp'):
         data['magnitude'] = calculate_magnitude(data['accel_x'], data['accel_y'], data['accel_z'])
 
         # Find the 5 maximum magnitudes and calculate their average
-        top_5_max_magnitudes = data.nlargest(5, 'magnitude')['magnitude']
+        top_5_max_magnitudes = data.nlargest(4, 'magnitude')['magnitude']
         average_max_magnitude = top_5_max_magnitudes.mean()
 
         # Print average magnitude value to Klipper console
         subprocess.run([f'echo M118 "Magnitude: {average_max_magnitude}" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
 
         # Delete the processed CSV file
-        os.remove(file_path)
+        #os.remove(file_path)
 
         return average_max_magnitude
 
@@ -111,13 +79,15 @@ def main():
     activate_and_measure_x()
     time.sleep(25)
     initial_magnitude = process_generated_csv()
+    microsteps = 0
+    magnitude_before_sync = initial_magnitude
     if initial_magnitude is not None:
         print(f"Initial Magnitude: {initial_magnitude}")
 
         # Send FORCE_MOVE_XONEPLUS command 3 times
         print("Sending FORCE_MOVE_XONEPLUS command...")
         force_move_xoneplus(3)
-            
+        microsteps = microsteps + 3    
         # Send ACTIVATE_AND_MEASURE_X command after movement
         print("Sending ACTIVATE_AND_MEASURE_X command after movement...")
         activate_and_measure_x()
@@ -141,14 +111,16 @@ def main():
                 steps = max(round(initial_magnitude / 10000), 1)
                 print("Sending FORCE_MOVE_XONEPLUS command...")
                 force_move_xoneplus(steps)
+                microsteps = microsteps + steps
                 activate_and_measure_x()
                 time.sleep(12)  # Adjust the delay time as needed
                 new_magnitude = process_generated_csv()
 
                 if new_magnitude > initial_magnitude:
                     force_move_xoneminus(steps)
+                    microsteps = microsteps - 1
                     print("Direction changed. Exiting the loop.")
-                    subprocess.run([f'echo M118 "X Motors synchronization completed" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
+                    subprocess.run([f'echo M118 "X Motors synchronization completed. Adjusted by {microsteps}/16 steps. Initial magnitude - {magnitude_before_sync}. Final magnitude - {initial_magnitude}" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
                     break
 
                 initial_magnitude = new_magnitude
@@ -158,14 +130,16 @@ def main():
                 steps = max(int(initial_magnitude / 10000), 1)
                 print("Sending FORCE_MOVE_XONEMINUS command...")
                 force_move_xoneminus(steps)
+                microsteps = microsteps - steps
                 activate_and_measure_x()
                 time.sleep(12)  # Adjust the delay time as needed
                 new_magnitude = process_generated_csv()
 
                 if new_magnitude > initial_magnitude:
                     force_move_xoneplus(steps)
+                    microsteps = microsteps + steps
                     print("Direction changed. Exiting the loop.")
-                    subprocess.run([f'echo M118 "X Motors synchronization completed" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
+                    subprocess.run([f'echo M118 "X Motors synchronization completed. Adjusted by {microsteps}/16 steps. Initial magnitude - {magnitude_before_sync}. Final magnitude - {initial_magnitude}" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
                     break
 
                 initial_magnitude = new_magnitude
@@ -175,13 +149,15 @@ def main():
     activate_and_measure_y()
     time.sleep(12)
     initial_magnitude = process_generated_csv()
+    microsteps = 0
+    magnitude_before_sync = initial_magnitude
     if initial_magnitude is not None:
         print(f"Initial Magnitude: {initial_magnitude}")
 
         # Send FORCE_MOVE_YONEPLUS command 3 times
         print("Sending FORCE_MOVE_YONEPLUS command...")
         force_move_yoneplus(3)
-            
+        microsteps = microsteps + 3
         # Send ACTIVATE_AND_MEASURE_Y command after movement
         print("Sending ACTIVATE_AND_MEASURE_Y command after movement...")
         activate_and_measure_y()
@@ -205,14 +181,16 @@ def main():
                 steps = max(round(initial_magnitude / 10000), 1)
                 print("Sending FORCE_MOVE_YONEPLUS command...")
                 force_move_yoneplus(steps)
+                microsteps = microsteps + steps
                 activate_and_measure_y()
                 time.sleep(12)  # Adjust the delay time as needed
                 new_magnitude = process_generated_csv()
 
                 if new_magnitude > initial_magnitude:
                     force_move_yoneminus(steps)
+                    microsteps = microsteps - steps
                     print("Direction changed. Exiting the loop.")
-                    subprocess.run([f'echo M118 "SYNCHRONIZATION COMPLETED ;)" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
+                    subprocess.run([f'echo M118 "Y Motors synchronization completed. Adjusted by {microsteps}/16 steps. Initial magnitude - {magnitude_before_sync}. Final magnitude - {initial_magnitude}" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
                     break
 
                 initial_magnitude = new_magnitude
@@ -222,18 +200,21 @@ def main():
                 steps = max(int(initial_magnitude / 10000), 1)
                 print("Sending FORCE_MOVE_YONEMINUS command...")
                 force_move_yoneminus(steps)
+                microsteps = microsteps - steps
                 activate_and_measure_y()
                 time.sleep(12)  # Adjust the delay time as needed
                 new_magnitude = process_generated_csv()
 
                 if new_magnitude > initial_magnitude:
                     force_move_yoneplus(steps)
+                    microsteps = microsteps + steps
                     print("Direction changed. Exiting the loop.")
-                    subprocess.run([f'echo M118 "SYNCHRONIZATION COMPLETED ;)" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
+                    subprocess.run([f'echo M118 "Y Motors synchronization completed. Adjusted by {microsteps}/16 steps. Initial magnitude - {magnitude_before_sync}. Final magnitude - {initial_magnitude}" > ~/printer_data/comms/klippy.serial'], check=True, shell=True)
                     break
 
                 initial_magnitude = new_magnitude
-
+    
+    resume_cmd()
 
 if __name__ == "__main__":
     main()
