915e32d0b133c0855ec6eef21696a126bbde5758
MRX8024 <57844100+MRX8024@users.noreply.github.com>
2024-07-19 05:05:26 +0300

    Merge plotter, full Cartesian support (chips support), live static measures, optimized median filter, new live variables, name corrections

--- motors_sync.py
+++ motors_sync.py
@@ -9,7 +9,6 @@ import numpy as np
 from . import z_tilt
 
 # MEASURE_DELAY = 0.25        # Delay between damped oscillations and measurement
-MAX_STEP_SIZE = 5           # Maximum number of microsteps move at a time
 AXES_LEVEL_DELTA = 2000     # Magnitude difference between axes
 MEDIAN_FILTER_WINDOW = 3    # Number of window lines
 
@@ -21,14 +20,14 @@ class MotorsSync:
         self.gcode_move = self.printer.load_object(config, 'gcode_move')
         self.force_move = self.printer.load_object(config, 'force_move')
         self.stepper_en = self.printer.load_object(config, 'stepper_enable')
-        self.printer.register_event_handler("klippy:connect", self.handler)
+        self.printer.register_event_handler("klippy:connect", self._handle_connect)
         self.status = z_tilt.ZAdjustStatus(self.printer)
         # Read config
-        self.accel_chip = self._init_chip()
         self.axes, self.do_level = self._init_axes()
-        self.solve_model, self.model_coeffs, self.extra_solve_model, self.extra_model_coeffs = self._init_model()
+        self.accel_chips = self._init_chips()
         self.microsteps = self.config.getint('microsteps', default=16, minval=2, maxval=32)
-        self.fast_threshold = self.config.getint('fast_threshold', default=999999, minval=0, maxval=999999)
+        self.solve_model, self.model_coeffs, self.extra_solve_model, self.extra_model_coeffs = self._init_model()
+        self.max_step_size = self.config.getint('max_step_size', default=5, minval=1, maxval=self.microsteps)
         self.retry_tolerance = self.config.getint('retry_tolerance', default=999999, minval=0, maxval=999999)
         self.max_retries = self.config.getint('retries', default=0, minval=0, maxval=10)
         self.debug = self.config.getboolean('debug', default=False)
@@ -37,7 +36,7 @@ class MotorsSync:
         self.gcode.register_command('SYNC_MOTORS_CALIBRATE', self.cmd_CALIBRATE_SYNC, desc='Calibrate synchronization')
         # Variables
 
-    def handler(self):
+    def _handle_connect(self):
         self.toolhead = self.printer.lookup_object('toolhead')
         self.travel_speed = self.toolhead.max_velocity / 2
         self.travel_accel = min(self.toolhead.max_accel, 5000)
@@ -53,12 +52,6 @@ class MotorsSync:
         if None in out: raise
         return out[0] if len(out) == 1 else out
 
-    def _init_chip(self):
-        try:
-            return self.config.get('accel_chip')
-        except Exception:
-            return self.lookup_config('resonance_tester', ['accel_chip'])
-
     def _init_axes(self):
         valid_axes = ['X', 'Y']
         kin = self.lookup_config('printer', ['kinematics'])
@@ -73,6 +66,12 @@ class MotorsSync:
         if any([axis not in valid_axes for axis in axes]):
             raise self.config.error('Invalid axes parameter')
         return axes, do_level
+
+    def _init_chips(self):
+        chips = {}
+        for axis in self.axes:
+            chips[axis] = self.config.get(f'accel_chip_{axis.lower()}')
+        return chips
 
     def _init_model(self):
         pol_models = ['linear', 'quadratic', 'cubic', 'quartic', 'quintic',
@@ -101,7 +100,7 @@ class MotorsSync:
                 raise self.config.error(f"{model.capitalize()}"
                                         f" model requires {config['args']['count']} coefficients")
         else:
-            raise self.config.error(f"Unknown model '{model}'")
+            raise self.config.error(f"Invalid model '{model}'")
 
     def polynomial_model(self, fx):
         if self.extra_solve_model:
@@ -139,20 +138,6 @@ class MotorsSync:
     def _stepper_move(self, stepper, dist):
         self.force_move.manual_move(stepper, dist, 100, self.travel_accel)
 
-    def _static_measure(self):
-        # Measure static vibrations
-        aclient = self.chip_config.start_internal_client()
-        time.sleep(0.250)
-        aclient.finish_measurements()
-        # Init data
-        vect = np.mean([
-            [sample.accel_x, sample.accel_y, sample.accel_z]
-            for sample in aclient.get_samples()], axis=0)
-        # Calculate static and find z axis for future exclude
-        self.z_axis = np.abs(vect[0:]).argmax()
-        xy_vect = np.delete(vect, self.z_axis, axis=0)
-        self.static_data = round(np.linalg.norm(xy_vect, axis=0), 2)
-
     def _buzz(self, axis):
         # Fading oscillations
         move_len = self.motion[axis]['move_len']
@@ -165,18 +150,24 @@ class MotorsSync:
 
     def _calc_magnitude(self, aclient):
         # Calculate impact magnitude
+        if self.debug: start_time = time.perf_counter()
         vect = np.array([[sample.accel_x, sample.accel_y, sample.accel_z]
                          for sample in aclient.get_samples()])
-        xy_vect = np.delete(vect, self.z_axis, axis=1)
+        cut = np.mean(vect[0:100, :], axis=0)
+        z_axis = np.abs(cut).argmax()
+        static = np.linalg.norm(cut[np.arange(3) != z_axis])
+        xy_vect = np.delete(vect, z_axis, axis=1)
         # Add window mean filter
-        magnitude = []
-        for i in range(int(MEDIAN_FILTER_WINDOW / 2), len(xy_vect) - int(MEDIAN_FILTER_WINDOW / 2)):
-            filtered_xy_vect = (np.median([xy_vect[i-int(MEDIAN_FILTER_WINDOW / 2)],
-                                xy_vect[i], xy_vect[i+int(MEDIAN_FILTER_WINDOW / 2)]], axis=0))
-            magnitude.append(np.linalg.norm(filtered_xy_vect))
+        half_window = MEDIAN_FILTER_WINDOW // 2
+        magnitudes = np.linalg.norm(np.array(np.median(
+            [xy_vect[i - half_window:i + half_window + 1]
+             for i in range(half_window, len(xy_vect) - half_window)], axis=1)), axis=1)
         # Return avg of 5 max magnitudes with deduction static
-        magnitude = np.mean(np.sort(magnitude)[-5:])
-        return round(magnitude - self.static_data, 2)
+        magnitude = np.mean(np.sort(magnitudes)[-5:])
+        if self.debug:
+            total_time = time.perf_counter()
+            self.gcode.respond_info(f'Static: {static:.2f}, calc time: {total_time - start_time:.6f} sec')
+        return np.around(magnitude - static, 2)
 
     def _measure(self, axis, buzz=True):
         # Measure the impact
@@ -184,7 +175,7 @@ class MotorsSync:
         if buzz: self._buzz(axis)
         self._stepper_switch(stepper, 1)
         self._stepper_switch(stepper, 0)
-        aclient = self.chip_config.start_internal_client()
+        aclient = self.motion[axis]['chip_config'].start_internal_client()
         # time.sleep(0.250)
         self.toolhead.dwell(0.05)
         self._stepper_switch(stepper, 1)
@@ -192,7 +183,7 @@ class MotorsSync:
         aclient.finish_measurements()
         return self._calc_magnitude(aclient)
 
-    def _prestart(self):
+    def _homing(self):
         # Homing and going to center
         now = self.printer.get_reactor().monotonic()
         kin_status = self.toolhead.get_kinematics().get_status(now)
@@ -229,12 +220,11 @@ class MotorsSync:
             while True:
                 if not m['move_dir'][1]:
                     self._detect_move_dir(max_ax)
-                buzz = m['magnitude'] <= self.fast_threshold
                 delta = int(self.solve_model(m['magnitude']) - self.solve_model(s['magnitude']))
-                m['move_msteps'] = min(max(delta, 1), MAX_STEP_SIZE)
+                m['move_msteps'] = min(max(delta, 1), self.max_step_size)
                 self._stepper_move(m['lookuped_steppers'][0], m['move_msteps'] * m['move_len'] * m['move_dir'][0])
                 m['actual_msteps'] += m['move_msteps'] * m['move_dir'][0]
-                m['new_magnitude'] = self._measure(max_ax, buzz)
+                m['new_magnitude'] = self._measure(max_ax, True)
                 self.gcode.respond_info(f"{max_ax}-New magnitude: {m['new_magnitude']} on"
                                         f" {m['move_msteps'] * m['move_dir'][0]}/{self.microsteps} step move")
                 if m['new_magnitude'] > m['magnitude']:
@@ -273,11 +263,10 @@ class MotorsSync:
             if not m['out_msg']:
                 if not m['move_dir'][1]:
                     self._detect_move_dir(axis)
-                buzz = m['magnitude'] <= self.fast_threshold
-                m['move_msteps'] = min(max(int(self.solve_model(m['magnitude'])), 1), MAX_STEP_SIZE)
+                m['move_msteps'] = min(max(int(self.solve_model(m['magnitude'])), 1), self.max_step_size)
                 self._stepper_move(m['lookuped_steppers'][0], m['move_msteps'] * m['move_len'] * m['move_dir'][0])
                 m['actual_msteps'] += m['move_msteps'] * m['move_dir'][0]
-                m['new_magnitude'] = self._measure(axis, buzz)
+                m['new_magnitude'] = self._measure(axis, True)
                 self.gcode.respond_info(f"{axis}-New magnitude: {m['new_magnitude']} on "
                                         f"{m['move_msteps'] * m['move_dir'][0]}/{self.microsteps} step move")
                 if m['new_magnitude'] > m['magnitude']:
@@ -309,19 +298,23 @@ class MotorsSync:
         self.motion = {}
         self.retries = 0
         # Live variables
-        accel_chip = gcmd.get('ACCEL_CHIP', self.accel_chip)
-        self.chip_config = self.printer.lookup_object(accel_chip)
-        self.fast_threshold = gcmd.get_int('FAST_THRESHOLD', self.fast_threshold, minval=0, maxval=999999)
+        axes_from_gcmd = gcmd.get('AXES', self.axes)
+        if any([axis.upper() not in self.axes for axis in axes_from_gcmd]):
+            raise self.gcode.error(f"Invalid axes parameter")
+        else:
+            self.axes = [axis.upper() for axis in axes_from_gcmd if axis.upper() in self.axes]
+        for axis in self.axes:
+            self.accel_chips[axis] = gcmd.get(f'ACCEL_CHIP_{axis}', self.accel_chips[axis])
         self.retry_tolerance = gcmd.get_int('RETRY_TOLERANCE', self.retry_tolerance, minval=0, maxval=999999)
         self.max_retries = gcmd.get_int('RETRIES', self.max_retries, minval=0, maxval=10)
         # Run
         self.status.reset()
-        self._prestart()
-        self._static_measure()
+        self._homing()
         self.gcode.respond_info('Motors synchronization started')
         # Init axes
         for axis in self.axes:
             self.motion[axis] = {}
+            self.motion[axis]['chip_config'] = self.printer.lookup_object(self.accel_chips[axis])
             stepper = 'stepper_' + axis.lower()
             self.motion[axis]['stepper'] = stepper
             self.motion[axis]['lookuped_steppers'] = [
@@ -334,11 +327,10 @@ class MotorsSync:
             self.motion[axis]['init_magnitude'] = self._measure(axis, True)
             self.motion[axis]['magnitude'] = self.motion[axis]['init_magnitude']
             self.motion[axis]['new_magnitude'] = 0
-            # self.motion[axis]['top'] = False
             self.motion[axis]['out_msg'] = ''
             self.gcode.respond_info(f"{axis}-Initial magnitude: {self.motion[axis]['init_magnitude']}")
         max_ax = ''
-        if self.do_level:
+        if self.do_level and len(self.axes) > 1:
             max_ax, min_ax = sorted(self.motion, key=lambda x: self.motion[x]['init_magnitude'], reverse=True)[:2]
             self._axes_level(min_ax, max_ax)
         self._final_sync(max_ax)
@@ -349,8 +341,120 @@ class MotorsSync:
 
     def cmd_CALIBRATE_SYNC(self, gcmd):
         # Calibrate sync model and model coeffs
-        from .motors_sync_plot import find_func
+        from datetime import datetime
+        from textwrap import wrap
         import multiprocessing
+        import matplotlib.pyplot as plt, matplotlib.ticker as ticker
+        from scipy.optimize import curve_fit
+
+        RESULTS_FOLDER = os.path.expanduser('~/printer_data/config/adxl_results/motors_sync')
+        RMSE_LIMIT = 20000
+
+        def check_export_path(path):
+            if not os.path.exists(path):
+                try:
+                    os.makedirs(path)
+                except OSError as e:
+                    raise self.gcode.error(f'Error generate path {path}: {e}')
+
+        def linear_model(x, a, b):
+            return a * x + b
+
+        def quadratic_model(x, a, b, c):
+            return a * x ** 2 + b * x + c
+
+        def power_model(x, a, b):
+            return a * np.power(x, b)
+
+        def root_model(x, a, b):
+            return a * np.sqrt(x) + b
+
+        def hyperbolic_model(x, a, b):
+            return a / x + b
+
+        def exponential_model(x, a, b, c):
+            return a * np.exp(b * x) + c
+
+        models = {
+            'Linear': linear_model,
+            'Quadratic': quadratic_model,
+            'Power': power_model,
+            'Root': root_model,
+            'Hyperbolic': hyperbolic_model,
+            'Exponential': exponential_model
+        }
+
+        linestyles = {
+            'Linear': '-.',
+            'Quadratic': '--',
+            'Power': ':',
+            'Root': '--',
+            'Hyperbolic': '-.',
+            'Exponential': ':'
+        }
+
+        colors = {
+            'Linear': '#DF8816',  # Dark Orange
+            'Quadratic': 'green',
+            'Power': 'cyan',
+            'Root': 'magenta',
+            'Hyperbolic': 'purple',
+            'Exponential': 'blue'
+        }
+
+        def find_best_func(x_data, y_data, accel_chip='', msteps=16):
+            maxfev = 999999999
+            params = {}
+            y_pred = {}
+            rmse = {}
+            for name, model in models.items():
+                params[name], _ = curve_fit(model, x_data, y_data, maxfev=maxfev)
+                y_pred[name] = model(x_data, *params[name])
+                rmse[name] = np.sqrt(np.mean((y_data - y_pred[name]) ** 2))
+            out = {}
+            for name, _ in models.items():
+                params_str = ','.join([f'{params[name][i]:.10f}' for i in range(len(params[name]))])
+                out[name] = {'val': rmse[name], 'params': params[name], 'equation': params_str}
+            sorted_out = sorted(out.keys(), key=lambda x: out[x]['val'])
+            string_cmd = ['Functions RMSE and coefficients']
+            for num, name in enumerate(sorted_out):
+                string_cmd.append(f'{name}: RMSE {out[name]["val"]:.0f} coeffs: {out[name]["equation"]}')
+            msg = plotter(out, sorted_out, x_data, y_data, accel_chip, msteps)
+            string_cmd.insert(0, msg)
+            return string_cmd
+
+        def plotter(out, sorted_out, x_data, y_data, accel_chip, msteps):
+            # Plotting
+            fig, ax = plt.subplots()
+            ax.scatter(x_data, y_data, label='Samples', color='red', zorder=2, s=10)
+            x_fit = np.linspace(min(x_data), max(x_data), 200)
+            for num, name in enumerate(sorted_out):
+                if out[name]["val"] < RMSE_LIMIT:
+                    string_graph = f'{name} RMSE: {out[name]["val"]:.0f}'
+                    linestyle = linestyles[name]
+                    linewidth = 1
+                    color = colors[name]
+                    ax.plot(x_fit, models[name](x_fit, *out[name]['params']),
+                            label=string_graph, linestyle=linestyle, linewidth=linewidth, color=color)
+            ax.legend(loc='lower right', fontsize=6, framealpha=1, ncol=1)
+            now = datetime.now().strftime('%Y%m%d_%H%M%S')
+            lognames = [now, '_' + accel_chip]
+            title = f"Dependency of desynchronization and functions ({''.join(lognames)})"
+            ax.set_title('\n'.join(wrap(title, 66)), fontsize=10)
+            ax.set_xlabel(f'Microsteps: 1/{msteps}')
+            ax.set_xticks(np.arange(0, max(x_data) + 2.5, 2.5))
+            ax.xaxis.set_minor_locator(ticker.MultipleLocator(2.5))
+            ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+            ax.set_ylabel('Magnitude')
+            ax.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+            ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+            ax.grid(which='major', color='grey')
+            ax.grid(which='minor', color='lightgrey')
+            check_export_path(RESULTS_FOLDER)
+            png_path = os.path.join(RESULTS_FOLDER, f'interactive_plot_{accel_chip}_{now}.png')
+            plt.savefig(png_path, dpi=1000)
+            return f'Access to interactive plot at: {png_path}'
+
         repeats = gcmd.get_int('REPEATS', 10, minval=1, maxval=100)
         peak_point = gcmd.get_int('PEAK_POINT', 50000, minval=10000, maxval=999999)
         # peak_point maxval - ? ~move_len * 6000000
@@ -404,7 +508,7 @@ class MotorsSync:
                 os.nice(20)
             except:
                 pass
-        msg = find_func(x_samples, y_samples, self.accel_chip, self.microsteps)
+        msg = find_best_func(x_samples, y_samples, self.accel_chips[axis], self.microsteps)
         for line in msg:
             self.gcode.respond_info(str(line))
 
